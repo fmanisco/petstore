db4380915b9659025cb3360fb821b2ec5726c8fe
fmanisco <francesco.manisco@eng.it>
2021-11-23 19:08:41 +0100

    utility

---
+++ src/main/java/it/beccaria/petstore/provini/NumGenerator.java
@@ -0,0 +1,13 @@
+package it.beccaria.petstore.provini;
+
+public class NumGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0; i<499; i++) {
+			System.out.println(i +1);
+		}
+	}
+
+}
